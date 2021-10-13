1a61643741e78bde62771b885c5807a38745cc29
Daniel Lerps <contact@lerps.de>
2021-10-13 16:29:01 +0200

    Scaffold project

---
+++ Playground/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Playground.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
